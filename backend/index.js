4527d319098dd4f3b13a45d881582147c2575044
kartikneelagund <kartikneelgund028@gmail.com>
2025-08-28 14:32:30 +0530

    17th commit

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,7 @@ import cors from 'cors';
 import authRoutes from './routes/auth.js';
 import blogRoutes from './routes/blog.js';
 import userRoutes from './routes/user.js';
-import authRoutes from "./routes/auth.js";
-import blogRoutes from "./routes/blog.js";
+
 
 dotenv.config(); // 👈 must come before process.env usage
 
